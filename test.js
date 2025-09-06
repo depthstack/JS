f052efb94bec27eef983abac1baac3478a631206
depthstack <rit609009@gmail.com>
2025-09-06 23:00:40 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+console.log("Hello")
+let a = "Shazan"
+//To uppercase
+b = a.toUpperCase()
+console.log(b)
+// new Number(100) to explicitly define the data type
+// to fixed 
+let c = 23333332
+console.log(c.toFixed(2))
+//tofixed is used to convert numbers to float to specific precisions like .tofixed(2) will give out 2 digits after decimal
+//.toLocaleString to insert commas fro number representation-
+console.log(c.toLocaleString('en-IN'))
+//++++++++++++++++++++++++++++++++MATHS+++++++++++++++++++++
+//Math.abs - modulus , .round- round off, .floor - round off to lower number g 4.3 ->4, .ceil - round of to upper integer eg. 4.3 -> 5 
+//Math.min(10,20,2,31) --> 2 , Math.max(10,2,31,45) --> 45
+//Math.random -> gives a number between 0 to 1 
